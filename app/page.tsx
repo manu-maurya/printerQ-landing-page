c069f4594cc54b2ee1cc8853638fa137d135a680
Manu H <manu07032004@gmail.com>
2025-11-26 15:11:12 +0530

    >

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
+// app/page.js
 import Hero from "./components/Hero";
+import GlassNavbar from "./components/NavBar"; // or wherever it is
 import HowItWorks from "./components/HowItWorks";
-import GlassNavbar from "./components/NavBar";
 
-export default function Home(){
-  return(
-    <div className="relative min-h-screen bg-p-cream w-full">
-      <GlassNavbar/>
-      <Hero/>
-      <HowItWorks/>
-    </div>
-  )
+export default function Home() {
+  return (
+    <main className="w-full relative">
+      
+      {/* Navbar sits on top (z-50) */}
+      <GlassNavbar />
+      
+      {/* Hero takes full viewport height (h-screen) */}
+      {/* id="home" allows the Navbar 'Home' link to scroll here */}
+      <section id="home">
+        <Hero />
+      </section>
+
+      {/* How It Works comes next */}
+      {/* Because we added scroll-mt-32, the navbar won't cover the title */}
+      <HowItWorks />
+
+    </main>
+  );
 }
